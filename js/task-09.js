dfcbfd026a5e90ec4adb3ef42843c91e91554b8d
OlexiyVr <olexiyvr@gmail.com>
2023-03-24 22:16:09 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const reffs = {
+  bodyEl: document.querySelector('body'),
+  buttonEl: document.querySelector(' .change-color'),
+  textEl: document.querySelector('.color'),
+};
+
+
+reffs.buttonEl.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  reffs.bodyEl.style.backgroundColor = color;
+  reffs.textEl.textContent = color;
+}
